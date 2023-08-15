d464c3612485df8c4925f5564571f8a9ec8351ea
nooni98 <newnypius.ted@gmail.com>
2023-08-15 13:18:46 +0530

    yay

---
+++ src/main/java/com/nooni19/leap/App.java
@@ -0,0 +1,13 @@
+package com.nooni19.leap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
